60c5274a1bd301f57dfcf37e745b7e16adcb951c
Zach Lefin <zachlefin@gmail.com>
2020-04-06 21:40:59 -0400

    init chandump

---
+++ chandump.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from sys import argv
+import re
+import urllib.request
+
+import requests
+import bs4
+
+
+def getfname(link):
+    pattern = re.compile(r'.*/(.*\..*)')
+    r = pattern.findall(link)
+    return r[0]
+
+
+def run():
+    r = requests.get(argv[1])
+    soup = bs4.BeautifulSoup(r.content)
+    thumbs = soup.findAll('a', {'class': 'fileThumb'})
+    for thumb in thumbs:
+        link = 'https:' + thumb.find('img')['src']
+        fname = getfname(link)
+        print('Downloading', fname)
+        urllib.request.urlretrieve(link, fname)
+
+    print('Done!')
+
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print('python chandump.py [THREAD URL]')
+        exit()
+    run()
